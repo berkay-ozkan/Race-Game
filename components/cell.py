ce971cefd2b57a5dc6b86e29a72971a252b9af53
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-11-30 00:09:35 +0300

    Add type hints to Cell instance variables

--- components/cell.py
+++ components/cell.py
@@ -5,6 +5,10 @@ from components import Car
 class Cell(Component):
     _attributes = Component._attributes | {"row": "int", "col": "int"}
 
+    def __init__(self) -> None:
+        self.row: int
+        self.col: int
+
     # Subclass instance functions
     def interact(self, car: Car) -> None:
         raise NotImplementedError
