aa10265fe5e74582837df1ec988b81f66803ea58
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-11-29 17:56:31 +0300

    Limit rotation to roads

--- components/cell.py
+++ components/cell.py
@@ -3,18 +3,7 @@ from components import Car
 
 
 class Cell(Component):
-    _attributes = Component._attributes | {
-        "rotation": "int",
-        "row": "int",
-        "col": "int"
-    }
-
-    def __init__(self) -> None:
-        super()
-        self.rotation: int
-
-    def draw(self) -> str:
-        return self._representation[self.rotation]
+    _attributes = Component._attributes | {"row": "int", "col": "int"}
 
     # Subclass instance functions
     def interact(self, car: Car) -> None:
--- components/cells/road.py
+++ components/cells/road.py
@@ -2,6 +2,16 @@ from components import Cell
 
 
 class Road(Cell):
+    _attributes = Cell._attributes | {
+        "rotation": "int",
+    }
+
+    def __init__(self) -> None:
+        super()
+        self.rotation: int
+
+    def draw(self) -> str:
+        return self._representation[self.rotation]
 
     def interact(self, car):
         car._speed *= 0.97  # reduce speed due to friction
