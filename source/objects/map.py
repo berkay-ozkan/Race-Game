2e5a1b3f325ed78e18a95c60ce829dbe46dc4f88
vusaltair <vusik547@gmail.com>
2024-12-06 23:44:10 +0300

    cars are now stopped when game mode is ended

--- source/objects/map.py
+++ source/objects/map.py
@@ -242,8 +242,12 @@ class Map(Monitor):
     def stop(self):
         if not self._game_mode_active:
             return
-
+        
         self._stop_event.set()
         self._game_thread.join()
+
+        for car in self._cars:
+            car.stop()
+        
         self._game_mode_active = False
         self._start_time = None
