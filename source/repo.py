0fe965697fea6a0738542b3ec7e79b1ad602bafd
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-12-08 00:01:35 +0300

    Fix Repo

--- source/repo.py
+++ source/repo.py
@@ -10,12 +10,12 @@ class Repo(Monitor):
 
     def __init__(self):
         super().__init__()
+        self.description = "repo"
+        self._id = ID_Tracker()._add_objects(self)
         self._attachments = {}
         self._objects = {}
         self.components = Component()
         self.components._id = ID_Tracker()._add_objects(self.components)
-        self._create_condition = self.CV()
-        self._attach_condition = self.CV()
 
     @Monitor.sync
     def create(self, **kwargs):
@@ -27,11 +27,7 @@ class Repo(Monitor):
         map = Map(description, cols, rows, cell_size, bg_color)
 
         id = ID_Tracker()._add_objects(map)
-        #self._objects[id] = map
         map._id = id
-        with self._create_condition:
-            self._create_condition.notify_all()
-
         return id
 
     @Monitor.sync
@@ -44,27 +40,12 @@ class Repo(Monitor):
 
     @Monitor.sync
     def attach(self, obj_id, user):
-
-        while obj_id not in ID_Tracker()._objects:
-           
-            with self._create_condition:
-                self._create_condition.wait()
-
         if obj_id not in self._attachments:
             self._attachments[obj_id] = set()
 
         self._attachments[obj_id].add(user)
 
-        with self._attach_condition:
-            self._attach_condition.notify_all()
-
         return ID_Tracker()._objects[obj_id]
-
-    @Monitor.sync
-    def get_map_for_user(self, user_id):
-        for map_id, users in self._attachments.items():
-            if user_id in users:
-                return map_id
 
     @Monitor.sync
     def list_attached(self, user):
@@ -78,31 +59,11 @@ class Repo(Monitor):
         if obj_id in self._attachments:
             if user in self._attachments[obj_id]:
                 self._attachments[obj_id].remove(user)
-                ID_Tracker()._objects[obj_id].remove_observer(user)
 
         if not self._attachments[obj_id]:
             del self._attachments[obj_id]
-
-        with self._attach_condition:
-            self._attach_condition.notify_all()
 
     @Monitor.sync
     def delete(self, obj_id):
         if obj_id not in self._attachments:
             del ID_Tracker()._objects[obj_id]
-
-        with self._create_condition:
-            self._create_condition.notify_all()
-
-    @Monitor.sync
-    def create_wait(self):
-        with self._create_condition:
-            
-            self._create_condition.wait()
-           
-    @Monitor.sync
-    def attach_wait(self):
-        with self._attach_condition:
-            
-            self._attach_condition.wait()
-            
