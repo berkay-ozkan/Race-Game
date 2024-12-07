cbbf99cf41f58d12f6b4f398df7dd3db5c386944
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-12-07 18:20:47 +0300

    Delete unused observer system

--- source/__init__.py
+++ source/__init__.py
@@ -2,6 +2,5 @@ from .objects.component import Component
 from .id_tracker import ID_Tracker
 from .objects import Map
 from .monitor import Monitor
-from .observer import Observer
 from .repo import Repo
 from .singleton import singleton
