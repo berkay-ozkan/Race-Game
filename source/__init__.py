ad89fe36619b08cb0194b48e32e35060a630f6d6
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-12-07 19:05:48 +0300

    Revert "Delete unused observer system"
    This reverts commit cbbf99cf41f58d12f6b4f398df7dd3db5c386944.

--- source/__init__.py
+++ source/__init__.py
@@ -2,5 +2,6 @@ from .objects.component import Component
 from .id_tracker import ID_Tracker
 from .objects import Map
 from .monitor import Monitor
+from .observer import Observer
 from .repo import Repo
 from .singleton import singleton
