7a92c6d301544919f21625f82944df00773766cb
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-12-20 20:43:59 +0300

    Add drafts of pages to be implemented

--- CENG445RaceGameContainer/CENG445RaceGame/views.py
+++ CENG445RaceGameContainer/CENG445RaceGame/views.py
@@ -51,3 +51,31 @@ def repo_list(request: HttpRequest):
     command = {"function_name": "list", "parameters": []}
     communicate_with_server(request, dumps(command))
     return redirect("/")
+
+
+repo_list_attached = NotImplemented
+repo_attach = NotImplemented
+repo_detach = NotImplemented
+repo_delete = NotImplemented
+component_factory_list = NotImplemented
+component_factory_create = NotImplemented
+component_factory_register = NotImplemented
+component_factory_unregister = NotImplemented
+object_getid = NotImplemented
+component_description = NotImplemented
+component_type = NotImplemented
+component_attributes = NotImplemented
+component_draw = NotImplemented
+map_getxy = NotImplemented
+map_place = NotImplemented
+map_view = NotImplemented
+map_draw = NotImplemented
+car_model = NotImplemented
+car_map = NotImplemented
+car_driver = NotImplemented
+car_pos = NotImplemented
+car_angle = NotImplemented
+car_topspeed = NotImplemented
+car_topfuel = NotImplemented
+car_speed = NotImplemented
+car_fuel = NotImplemented
