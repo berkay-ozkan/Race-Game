f10da053d30e75a09eb68a68c041e07aed5c566a
Berkay Özkan <berkay.ozkan@ceng.metu.edu.tr>
2024-12-22 19:26:53 +0300

    Create backend application

--- CENG445RaceGame/settings.py
+++ CENG445RaceGame/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'frontend',
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
